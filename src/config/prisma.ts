56aebce949cc87a4853ef8ff7e78a04827873228
AndresSanchez <sanchezandres8203@gmail.com>
2026-04-08 00:46:35 -0400

    fix: new prisma arch

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeonHttp } from '@prisma/adapter-neon';
+import { ENV } from './env';
+
+function createPrismaClient() {
+  const adapter = new PrismaNeonHttp(ENV.DATABASE_URL, {});
+  return new PrismaClient({ adapter });
+}
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ['error'] });
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (ENV.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
